79ce0468aad5aac8b97b1e3c866313c05b99d23b
Rafa Osnaya <osnayadeluciorafael@hotmail.com>
2022-08-08 21:50:30 -0500

    Aplicando filter para marca & año

--- js/app.js
+++ js/app.js
@@ -33,29 +33,32 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 //Eventos de los select de busqueda
-marca.addEventListener('change', e =>{
+marca.addEventListener('change', e => {
     datosBusqueda.marca = e.target.value;
+    filtrarAuto();
 })
 
-year.addEventListener('change',e =>{
-    datosBusqueda.year = e.target.value;
+year.addEventListener('change', e => {
+    datosBusqueda.year = parseInt(e.target.value);
+    filtrarAuto();
 })
 
-minimo.addEventListener('change', e =>{
+minimo.addEventListener('change', e => {
     datosBusqueda.minimo = e.target.value;
 })
 
-maximo.addEventListener('change', e =>{
+maximo.addEventListener('change', e => {
     datosBusqueda.maximo = e.target.vale;
 })
-puertas.addEventListener('change',e => {
+puertas.addEventListener('change', e => {
     datosBusqueda.puertas = e.target.value;
 })
-transmision.addEventListener('change', e =>{
+transmision.addEventListener('change', e => {
     datosBusqueda.transmision = e.target.vale;
 })
-color.addEventListener('change',e =>{
-    datosBusqueda.color = e.target.value;  
+color.addEventListener('change', e => {
+    datosBusqueda.color = e.target.value;
+    console.log(datosBusqueda);
 })
 
 
@@ -92,4 +95,24 @@ function yearsSelect() {
     }
 }
 
+//Funcion de alto nivel
+function filtrarAuto() {
+    const resultado = autos.filter(filtrarMarca).filter(filtrarYear);
+    console.log(resultado);
+}
 
+function filtrarMarca(auto) {
+    const { marca } = datosBusqueda;
+    if (marca) {
+        return auto.marca === marca;
+    }
+    return auto;
+}
+
+function filtrarYear(auto) {
+    const { year } = datosBusqueda;
+    if (year) {
+        return auto.year === year;
+    }
+    return auto;
+}
